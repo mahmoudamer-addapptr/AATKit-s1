0dd2ffe6feb8f5cc8e96216312891787799862d4
Mahmoud Amer <mahmoud.amer@addapptr.com>
2025-09-10 10:42:45 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -29,20 +29,20 @@ let package = Package(
         .binaryTarget(
             name: "AATKit",
             url:
-                "https://ios-sdk.aatkit.com/AATKit_SPM/AATKit.zip",
+                "https://ios-sdk.aatkit.com/AATKit_SPM/\(version)/AATKit.zip",
             checksum: "da47fe8cbb70c13972aa7d49833b1120742a6542656436164cf8a334170d6ecb"
         ),
         // FeedAd
         .binaryTarget(
             name: "TappxFramework",
             url:
-                "https://ios-sdk.aatkit.com/AATKit_SPM/TappxFramework.zip",
+                "https://ios-sdk.aatkit.com/AATKit_SPM/\(version)/TappxFramework.zip",
             checksum: "917fa7e5df22c86da30e861966e2c43c83809642d4953091c9af53179965d266"
         ),
         .binaryTarget(
             name: "AATTappxAdapter",
             url:
-                "https://ios-sdk.aatkit.com/AATKit_SPM/AATTappxAdapter.zip",
+                "https://ios-sdk.aatkit.com/AATKit_SPM/\(version)/AATTappxAdapter.zip",
             checksum: "e8513cdebc1580adfa6ee45978711ff5062541e0d3e45f3579f714d80d7f51ab"
         )
     ]
